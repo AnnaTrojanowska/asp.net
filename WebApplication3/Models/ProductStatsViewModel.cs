eeae9cf01d282b5ad9eedba10a6ceb88460abef2
AnnaTrojanowska <anna.trojanowska1@microsoft.wsei.edu.pl>
2021-10-11 13:24:34 +0200

    lab2

---
+++ WebApplication3/Models/ProductStatsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class ProductStatsViewModel
+    {
+        public int NameLength { get; internal set; }
+        public int DescriptionLength { get; internal set; }
+    }
+}
